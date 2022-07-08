a071badefd30d2f8fe39e1c0b84a940156258ee0
Thumpudi Vishnu <87056972+vishnuthumpudi@users.noreply.github.com>
2022-07-08 22:24:33 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,85 @@
+body{border-width:100px;
+	}
+h2{ padding-top:10px;
+	color:#FF4838;
+	}
+p{color:salmon;}
+.inform{padding:4px;
+	background-color: #F1EB9C;
+	border-radius:10px;
+	border:4px solid gold;
+	box-shadow: 5px 5px #ADFF2F;
+}
+h3{
+	color:aqua;}
+tr{margin-right:0px;
+	padding:0px;
+	}
+table {border-radius: 9px;
+           border: 2px solid blue;
+		   padding:3px 3px 3px 4px;
+		   margin:0px 0px 0px 0px;
+		   
+		   
+		   
+}
+button
+{
+	padding:1px 2px 1px 2px;
+	border:1px dotted salmon;
+	font:inherit;
+	background-color:aqua;
+	color:black;
+	cursor:pointer;
+	border-radius:10px;
+	justify-content:center;
+	align-items:center;
+	display:flex;
+}
+table {
+  table-layout: fixed ;
+  
+}
+td {
+  width: 25% ;
+}
+marquee{
+}
+td{
+	border-radius: 9px;
+           border: 2px dashed blue;
+		   
+}
+body, html {
+    margin: 0px;
+    padding: 0px;
+    background-color: #ededed;
+    font-family: 'Trebuchet MS';
+}
+.redirecter{
+	color:#F51720;
+}
+.ap12{
+	background:url("qwer1.png");
+	background-size: cover;
+ background-repeat: no-repeat;
+	color:green;
+	
+}
+th{
+	width:300px;
+}
+a:hover
+{
+	background-color:yellow;
+}
+a{
+underline:none;
+color:none;
+text-decoration:none;
+}
+iframe{target-new:window;
+	border-radius:40px;
+	
+	padding: 8px;
+}
